2a5eff3b6c614fb2d5d834abb7ed96ed34077425
Daniel Platz <daniel.platz@me.com>
2018-05-30 18:52:12 +0200

    Updated image-name

--- hello/build-docker-image.sh
+++ hello/build-docker-image.sh
@@ -3,5 +3,5 @@ set -e
 
 ./gradlew build
 
-docker build -t dplatz/hello .
-#docker run --rm --name appsvr -p 80:8080 dplatz/hello
+docker build -t 38leinad/hello .
+#docker run --rm --name appsvr -p 80:8080 38leinad/hello
